7a401ba3db0eeca2350181c6bd2a0a982f63610e
ryuki <ryuki@localhost>
2025-12-01 00:42:08 +0900

    課題２９修正

--- curriculum/src/Ques29/main2.java
+++ curriculum/src/Ques29/main2.java
@@ -4,31 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import Map.map;
+import map.MapData;
 
 public class main2 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        map[] data = {
-            new map("北海道", "札幌市", 83424),
-            new map("青森県", "青森市", 9646),
-            new map("岩手県", "盛岡市", 15275),
-            new map("宮城県", "仙台市", 7282),
-            new map("秋田県", "秋田市", 11638),
-            new map("山形県", "山形市", 9323),
-            new map("福島県", "福島市", 13784),
-            new map("茨城県", "水戸市", 6097),
-            new map("栃木県", "宇都宮市", 6408),
-            new map("群馬県", "前橋市", 6362),
-            new map("埼玉県", "さいたま市", 3798)
+        MapData[] data = {
+            new MapData("北海道", "札幌市", 83424),
+            new MapData("青森県", "青森市", 9646),
+            new MapData("岩手県", "盛岡市", 15275),
+            new MapData("宮城県", "仙台市", 7282),
+            new MapData("秋田県", "秋田市", 11638),
+            new MapData("山形県", "山形市", 9323),
+            new MapData("福島県", "福島市", 13784),
+            new MapData("茨城県", "水戸市", 6097),
+            new MapData("栃木県", "宇都宮市", 6408),
+            new MapData("群馬県", "前橋市", 6362),
+            new MapData("埼玉県", "さいたま市", 3798)
         };
 
         String input = sc.nextLine();
         String[] parts = input.split(",");
 
-        List<map> pickedList = new ArrayList<>();
+        List<MapData> pickedList = new ArrayList<>();
         for (String s : parts) {
             try {
                 int index = Integer.parseInt(s.trim());
@@ -51,7 +51,7 @@ public class main2 {
             pickedList.sort((a, b) -> Double.compare(a.getArea(), b.getArea())); // 降順：大→小
         }
 
-        for (map p : pickedList) {
+        for (MapData p : pickedList) {
             p.printInfo();
         }
 
