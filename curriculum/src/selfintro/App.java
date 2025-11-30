3e994dd0f8503457e24b64f7f6c1d97526314708
ryuki <ryuki@localhost>
2025-12-01 00:20:03 +0900

    課題３２

--- curriculum/src/selfintro/App.java
+++ curriculum/src/selfintro/App.java
@@ -1,15 +1,13 @@
 package selfintro;
 
 class App {
-  public static void main(String[] args) {
+  public static void main(String[] args){
 
-    Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+    Person person1 = new Person("鈴木", "太郎", 20, 1.7, 60);
     person1.print();
 
-    Person person2 = new Person("山田花子", 22, 1.5, 40);
+    Person person2 = new Person("山田", "花子", 22, 1.5, 40);
     person2.print();
-
-    System.out.println("合計" + Person.count + "人です");
 
     Person.printCount();
   }
--- curriculum/src/selfintro/Person.java
+++ curriculum/src/selfintro/Person.java
@@ -1,32 +1,45 @@
 package selfintro;
 
 class Person {
-
-  private String name;
+  private static int count = 0;
+  private String firstName;
+  private String lastName;
   private int age;
   private double height;
   private double weight;
 
-  static int count = 0;
-
-  Person(String name, int age, double height, double weight){
-    this.name = name;
+  Person(String firstName, int age, double height, double weight){
+    this.firstName = firstName;
     this.age = age;
     this.height = height;
     this.weight = weight;
     count++;
   }
 
+  Person(String firstName, String lastName, int age, double height, double weight){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+    count++;
+  }
+
+  public String fullName(){
+    return this.firstName + this.lastName;
+  }
+
+  public void print(){
+    System.out.println("名前は" + this.fullName() + "です");
+    System.out.println("年は" + this.age + "です");
+  }
+
   public double bmi(){
     return this.weight / this.height / this.height;
   }
 
-  public void print(){
-    System.out.println("名前は" + this.name + "です");
-    System.out.println("年は" + this.age + "です");
-  }
-
   public static void printCount(){
-    System.out.println("合計" + count + "人です");
+    System.out.println("合計" + Person.count + "人です");
   }
 }
+
