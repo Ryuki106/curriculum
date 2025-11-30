d2b0bccdffd22e4b963f1414236efa07dd629386
ryuki <ryuki@localhost>
2025-12-01 00:12:28 +0900

    課題３１

--- curriculum/src/selfintro/Person.java
+++ curriculum/src/selfintro/Person.java
@@ -2,30 +2,31 @@ package selfintro;
 
 class Person {
 
-  String name;
-  int age;
-  double height;
-  double weight;
+  private String name;
+  private int age;
+  private double height;
+  private double weight;
 
   static int count = 0;
 
-  Person(String name, int age, double height, double weight) {
+  Person(String name, int age, double height, double weight){
     this.name = name;
     this.age = age;
     this.height = height;
     this.weight = weight;
-
     count++;
   }
 
-  double bmi() {
-    return this.weight / (this.height * this.height);
+  public double bmi(){
+    return this.weight / this.height / this.height;
   }
 
-  void print() {
+  public void print(){
     System.out.println("名前は" + this.name + "です");
     System.out.println("年は" + this.age + "です");
-    System.out.println("BMIは" + String.format("%.2f", this.bmi()) + "です");
+  }
+
+  public static void printCount(){
     System.out.println("合計" + count + "人です");
   }
 }
