dc78382221703fb9ae9e133fe306f9e17cb82370
ryuki <ryuki@localhost>
2025-12-01 00:03:36 +0900

    課題３０

---
+++ curriculum/src/selfintro/App.java
@@ -0,0 +1,10 @@
+package selfintro;
+
+class App {
+  public static void main(String[] args) {
+
+    Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+
+    person1.print();
+  }
+}
---
+++ curriculum/src/selfintro/Person.java
@@ -0,0 +1,31 @@
+package selfintro;
+
+class Person {
+
+  String name;
+  int age;
+  double height;
+  double weight;
+
+  static int count = 0;
+
+  Person(String name, int age, double height, double weight) {
+    this.name = name;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+
+    count++;
+  }
+
+  double bmi() {
+    return this.weight / (this.height * this.height);
+  }
+
+  void print() {
+    System.out.println("名前は" + this.name + "です");
+    System.out.println("年は" + this.age + "です");
+    System.out.println("BMIは" + String.format("%.2f", this.bmi()) + "です");
+    System.out.println("合計" + count + "人です");
+  }
+}
